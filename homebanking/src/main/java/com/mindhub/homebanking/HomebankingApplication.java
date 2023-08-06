97da844b74cf2b622d2de890dbc11cdf944ed4ec
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-08-06 13:27:30 -0300

    arregle errores en branch task_1

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,8 +1,6 @@
 package com.mindhub.homebanking;
 
-import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,25 +17,10 @@ public class HomebankingApplication {
 		System.out.println("STARTED!!");
 	}
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository) {
+	public CommandLineRunner initData(ClientRepository clientRepository) {
 		return (args) -> {
 			Client client1 = new Client("Melba", "Morel","melba@mindhub.com");
 			clientRepository.save(client1);
-
-			Client client2 = new Client("Pablo", "Moya","pablomoya@mindhub.com");
-			clientRepository.save(client2);
-			LocalDate today = LocalDate.now();
-
-			Account account3 = new Account("132456b",today,2000,client2);
-			accountRepository.save(account3);
-
-
-			Account account1 = new Account("1a2b",today,5000,client1);
-			accountRepository.save(account1);
-
-			LocalDate tomorrow = today.plusDays(1);
-			Account account2 = new Account("789012", tomorrow, 7500,client1);
-			accountRepository.save(account2);
 		};
 	}
 }
