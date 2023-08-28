30456d1074518dd9e90ae44a80004370d9917fe0
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-08-28 12:49:13 -0300

    accountService y su implementaciÃ³n creado, desvinculado accountRepository de accountRepository

--- homebanking/src/main/java/com/mindhub/homebanking/services/AccountService.java
+++ homebanking/src/main/java/com/mindhub/homebanking/services/AccountService.java
@@ -1,5 +1,15 @@
 package com.mindhub.homebanking.services;
 
+import com.mindhub.homebanking.DTO.AccountDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public interface AccountService {
+    List<AccountDTO> getAccounts();
+    ResponseEntity<AccountDTO> getAccount(@PathVariable Long id);
+    ResponseEntity<Object> createAccount(Authentication authentication);
     String generateAccountNumber();
 }
--- homebanking/src/main/java/com/mindhub/homebanking/services/implement/AccountImplService.java
+++ homebanking/src/main/java/com/mindhub/homebanking/services/implement/AccountImplService.java
@@ -1,15 +1,63 @@
 package com.mindhub.homebanking.services.implement;
 
+import com.mindhub.homebanking.DTO.AccountDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import com.mindhub.homebanking.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountImplService implements AccountService {
     @Autowired
-    private AccountService accountService;
+    private AccountRepository accountRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public ResponseEntity<AccountDTO> getAccount(@PathVariable Long id){
+        return accountRepository.findById(id)
+                .map(AccountDTO::new)
+                .map(ResponseEntity::ok)
+                .orElse(null);
+    }
+
+    @Override
+    public List<AccountDTO> getAccounts(){
+        return accountRepository.findAll().stream()
+                .map(AccountDTO::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public ResponseEntity<Object> createAccount(Authentication authentication){
+        LocalDate today = LocalDate.now();
+
+        Client client = clientRepository.findByEmail(authentication.getName());
+
+        if (client == null) {
+            return new ResponseEntity<>("Client not found", HttpStatus.NOT_FOUND);
+        }
+        if (client.getAccounts().size() >= 3) {
+            return new ResponseEntity<>("Maximum account limit reached", HttpStatus.FORBIDDEN);
+        }
+
+        String accountNumber = generateAccountNumber();
+        Account newAccount = new Account(accountNumber,today,0.0,client);
+        accountRepository.save(newAccount);
+
+        return new ResponseEntity<>("Account successfully created", HttpStatus.CREATED);
+    }
 
     @Override
     public String generateAccountNumber(){
